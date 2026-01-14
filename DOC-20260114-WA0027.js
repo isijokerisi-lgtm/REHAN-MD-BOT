2de050963ee660dcfb133da0d19bbe8195267f18
isijokerisi-lgtm <isijokerisi@gmail.com>
2026-01-14 14:00:38 +0500

    Add files via upload

---
+++ DOC-20260114-WA0027.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'GHOST BOT MD',
+  author: '‎',
+  botName: "GHOST BOT MD",
+  botOwner: 'REHAN', // 
+  ownerNumber: '923259487091', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.5",
+  updateZipUrl: "https://github.com/talhaewqr/SILVER-XD",, //SET YOUR REPO LINK
+};
+
+module.exports = settings;
